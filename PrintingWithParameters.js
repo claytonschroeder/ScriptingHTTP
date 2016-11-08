3ad3a99a17c68fd0eda6ad2b271390b7291f09b7
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2016-11-08 00:08:07 +0000

    Added modularization feature, allowing a var/object to be passed through print function

--- PrintingWithParameters.js
+++ PrintingWithParameters.js
@@ -1,16 +1,23 @@
 var https = require('https');
 
+    var requestOptions1 = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+    var requestOptions2 = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+    var requestOptions3 = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step3.html'
+    };
 
+function getAndPrintHTML (options) {
 
-function getAndPrintHTML () {
-
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
-  };
   // notice that https.get takes a callback with one parameter -
   // response, which is a Stream that represents the HTTP response
-  https.get(requestOptions, function (response) {
+  https.get(options, function (response) {
 
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
@@ -34,4 +41,4 @@ function getAndPrintHTML () {
 
 }
 
-getAndPrintHTML();
+getAndPrintHTML(requestOptions1);
